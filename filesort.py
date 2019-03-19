9579f329a9e12734780e76e97161bc4f951e8e90
iamoziroz <nickpap556@gmail.com>
2019-03-19 23:23:14 +0200

    Update filesort.py
    removed test output and added a disk_acces_counter

--- filesort.py
+++ filesort.py
@@ -8,10 +8,12 @@ byte_array = bytearray(number_file.read())  # TODO: change it so that it loads o
 file_pointer = 0  # a "pointer" to keep our position in the "file"(NOT in the 10,000 array)
 array_to_sort = []  # initialise an empty list
 buffer_size = 1000
+disk_acess_counter = 0
 
 for l in range(10):
     for i in range(10):
         buffer = [int(byte_array[x]) for x in range(file_pointer,(file_pointer + buffer_size))]  # this buffer consists of integers
+        disk_acess_counter = disk_acess_counter + 1  # this is counted as an access on the fisk
         file_pointer = file_pointer + buffer_size  # move our position(RETAINS ITS VALUE ON THE OUTER LOOP)
         for index in range(buffer_size):  # fill the buffer with the array_to_sort contents
             array_to_sort.append(buffer[index])
@@ -32,18 +34,13 @@ for l in range(10):
         array_pointer = array_pointer + buffer_size  # this array pointer is retained through this loop(k loop)
         byte_buffer = bytearray(buffer)  # convert the buffer to bytearray
         sorted_file.write(byte_buffer)  # writes(appends) the buffer to the i-th sorted file
+        disk_acess_counter = disk_acess_counter + 1
     print("created new " + filename )
     sorted_file.close()
 print("Done.")
 
-filename_to_test = "sorted_file0"  # change file name to test a different file
-print("Testing " + filename_to_test +"... \n")
-print("Printing first 100 numbers... ")
-number_file = open(filename_to_test, 'rb')
-byte_array = bytearray(number_file.read())
-for i in range(100):
-    print(str(int(byte_array[i])) + "," , end = '')
-print("\nIf the numbers seem sorted, chances are that the sorting was successful.")
+print("Acessed the disk a total of " + str(disk_acess_counter) + " times.")
+
 
 
 
