76a58bd0b4acc6feacde4a78eaa7d01001c3e98d
iamoziroz <nickpap556@gmail.com>
2019-03-20 15:48:14 +0200

    Update filesort_merge.py
    SEEEEms to work

--- filesort_merge.py
+++ filesort_merge.py
@@ -10,8 +10,9 @@ def read_buffer_from_file(file_number,file_pointer,buffer_size):  # reads the de
         sorted_file.seek(file_pointer)  # sets the beginning of our reading space on the file at position file_pointer
         byte_array = bytearray(sorted_file.read(buffer_size))  # reads buffer_size amount of bytes from the file
         sorted_file.close()
-        for i in range(file_pointer, file_pointer + buffer_size):  # fill the buffer from the file
-             buffer.append(byte_array[i])
+        for i in range( buffer_size):  # fill the buffer from the file TODO CHECK (-1)
+            print(" i = " + str(i))
+            buffer.append(byte_array[i])
         return buffer
     else:
         print("error while reading from file: file_pointer>9000")
@@ -32,6 +33,7 @@ list_of_files = [0,1,2,3,4,5,6,7,8,9]                                           
 position_of_min_val = 0                                                                                     #
 buffer11 = []                                                                                               #
 disk_access_counter = 0                                                                                     #
+temp_list = [0 for d in range(10)]                                                                                              #
 #############################################################################################################
 
 for initial_fill_counter in range(10):  # initialise all the buffers once
@@ -45,45 +47,41 @@ if os.path.exists("final_file"):  # check if there is an old existing file...
 
 # M A I N   B I G   L O O P
 for main_counter in range(N):
-    min_val = buffer[list_of_files[list_of_files[0]]][buffer_pointer[list_of_files[0]]]  # initialise with the first element of the first buffer that exists in the available files
-    print("MinVal = " + str(min_val))
-    #finds the lesser value from the buffers
-    for min_val_counter in list_of_files:
-        if buffer[min_val_counter][buffer_pointer[min_val_counter]] < min_val:
-            min_val = buffer[min_val_counter][buffer_pointer[min_val_counter]]
-            buffer_pointer[min_val_counter] = buffer_pointer[min_val_counter] + 1
-            position_of_min_val = min_val_counter
+    print("maincounter=" + str(main_counter))
+    for i in list_of_files:
+        temp_list[i] = buffer[i][buffer_pointer[0]]
+    # now temp_list holds all the numbers that need to be compared(CONFIRMED!)
+    min_val = min(temp_list)
+    print("MinVal= " + str(min_val))
+    position_of_min_val = temp_list.index(min_val)
 
-    # IMPORTAND: the only change is hapening to buffer No. position_of_min_val, so from now on we only use that variable on our checks.
-    # CHECKS if the current file/buffer is emptied
-    if buffer_pointer[position_of_min_val] == buffer_size:  # if the buffer is "empty"(pointer had reached the end)
-        if file_pointer[position_of_min_val] < 10000:
-            buffer[position_of_min_val] = read_buffer_from_file(position_of_min_val,file_pointer[position_of_min_val],buffer_size)
-            buffer_pointer[position_of_min_val] = 0
-            file_pointer[position_of_min_val] = file_pointer[position_of_min_val] + buffer_size
-        else:
-            list_of_files.remove(position_of_min_val)
-            filename = "sorted_file" + str(position_of_min_val)
-            os.remove(filename)
-            print("Removed sorted_file" + str(position_of_min_val) + " from file list.")
+    # increase buffer pointer
+    buffer_pointer[position_of_min_val] = buffer_pointer[position_of_min_val] + 1
 
-    buffer11.append(buffer[position_of_min_val][buffer_pointer[position_of_min_val]])  # adds the minimum value to the buffer11
-    # checks if the buffer11 is full to write it to the final file and empty it
-    if len(buffer11) == buffer_size :
+    # write to buffer11
+    buffer11.append(min_val)
+
+    # check buffer11
+    if len(buffer11) == 1000 :
         write_buffer11_to_file(buffer11)
         disk_access_counter = disk_access_counter + 1
         buffer11.clear()
 
-print("\n\n\nTotal disk_acces_counter=" + str(disk_access_counter))
+    # check bufferX
+    if buffer_pointer[position_of_min_val] == (buffer_size - 1):
+        buffer[position_of_min_val] = read_buffer_from_file(position_of_min_val,file_pointer[position_of_min_val],buffer_size)
+        disk_access_counter = disk_access_counter + 1
+        file_pointer[position_of_min_val] = file_pointer[position_of_min_val] + buffer_size
+        buffer_pointer[position_of_min_val] = 0
 
-filename_to_test = "final_file"  # change file name to test a different file
-print("\nTesting " + filename_to_test +"... \n")
-print("\nPrinting first 1000 numbers... ")
-number_file = open(filename_to_test, 'rb')
-byte_array = bytearray(number_file.read())
-for i in range(10000):
-    print(str(int(byte_array[i])) + "," , end = '')
-print("\nIf the numbers seem sorted, chances are that the sorting was successful.")
+    # check fileX
+    if file_pointer[position_of_min_val] >=10000:
+        list_of_files.remove(position_of_min_val)
+        filename = "sorted_file" + str(position_of_min_val)
+        os.remove(filename)
+
+print("\n\n\nTotal disk_access_counter = " + str(disk_access_counter))
+
 
 
 
